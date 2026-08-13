9b40b8bc617d395bacff591a0a96bc836377f9d5
YourTube Dev <dev@example.com>
2026-08-13 13:06:46 +0530

    Update Next.js to latest version

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCyxbdclt2ocA5zgE-MDy1ndYIFqVMAr30",
+  authDomain: "yourtube-8cda9.firebaseapp.com",
+  projectId: "yourtube-8cda9",
+  storageBucket: "yourtube-8cda9.firebasestorage.app",
+  messagingSenderId: "921641878423",
+  appId: "1:921641878423:web:0d65801eebaf2b25f03ad2"
 };
 
 const app = initializeApp(firebaseConfig);
